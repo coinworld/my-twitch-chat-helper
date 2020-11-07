481e1642de92e14ef4780ade3decadb670f06409
coinworld <hwsnemo@gmail.com>
2020-11-07 14:49:25 +0900

    Fix default NP format

--- src/main/java/tv/twitch/hwsnemo/autoreply/cmd/impl/NpCmd.java
+++ src/main/java/tv/twitch/hwsnemo/autoreply/cmd/impl/NpCmd.java
@@ -10,7 +10,7 @@ import tv.twitch.hwsnemo.autoreply.osu.gosu.NowPlaying;
 
 public class NpCmd implements Cmd {
 
-	private static String format = "%1$s - %2$s [%3$s] +%9$s (by %4$s | %5$f* | %6$s) %10$s";
+	private static String format = "%1$s - %2$s [%3$s] +%9$s (by %4$s | %5$.2f* | %6$s) %10$s";
 
 	// "{artist} - {song} [{difficulty}] +{mods} (by {mapper} | {sr}* | {bpm}) osu.ppy.sh/s/{setid}" {beatmapid} - 8
 
