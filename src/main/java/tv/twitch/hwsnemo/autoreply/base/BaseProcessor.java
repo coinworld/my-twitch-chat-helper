19612b80c6b72100a33d3a8f5aafc333aa4618ca
coinworld <hwsnemo@gmail.com>
2021-05-08 13:17:30 +0900

    Better generic usage

--- src/main/java/tv/twitch/hwsnemo/autoreply/base/BaseProcessor.java
+++ src/main/java/tv/twitch/hwsnemo/autoreply/base/BaseProcessor.java
@@ -1,16 +1,15 @@
 package tv.twitch.hwsnemo.autoreply.base;
 
-public abstract class BaseProcessor<T extends BaseMap<?>, V, A extends BaseEntry<?>> {
+public abstract class BaseProcessor<T extends BaseMap<A>, V, A extends BaseEntry<?>> {
 	protected final T map;
 	protected BaseProcessor(T map) {
 		this.map = map;
 	}
 	
-	@SuppressWarnings("unchecked")
 	public boolean loop(V arg) {
-		for (BaseEntry<?> ent : map.getEntries()) {
+		for (A ent : map.getEntries()) {
 			if (ent.isEnabled()) {
-				if (pass((A) ent, arg)) {
+				if (pass(ent, arg)) {
 					return true;
 				}
 			}
