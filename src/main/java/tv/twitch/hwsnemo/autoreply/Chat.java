cfe1e186dae299a2017582a2823c8bda334aaa92
coinworld <hwsnemo@gmail.com>
2021-05-09 11:32:52 +0900

    Deal with possible disconnection

--- src/main/java/tv/twitch/hwsnemo/autoreply/Chat.java
+++ src/main/java/tv/twitch/hwsnemo/autoreply/Chat.java
@@ -10,6 +10,7 @@ import org.pircbotx.Configuration;
 import org.pircbotx.PircBotX;
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.ConnectEvent;
+import org.pircbotx.hooks.events.DisconnectEvent;
 import org.pircbotx.hooks.events.MessageEvent;
 
 import tv.twitch.hwsnemo.autoreply.cmd.ChatCmdInfo;
@@ -34,7 +35,7 @@ public class Chat {
 
 			// Main.revert();
 			// do not let System.out.println be used
-			
+
 			Main.write("Connected. Ctrl+C to exit.");
 			PircBotX bot = event.getBot();
 
@@ -42,35 +43,38 @@ public class Chat {
 			bot.sendRaw().rawLineNow("CAP REQ :twitch.tv/tags");
 			bot.sendRaw().rawLineNow("CAP REQ :twitch.tv/membership");
 			bot.sendRaw().rawLineNow("CAP REQ :twitch.tv/tags twitch.tv/commands");
-
-			while (true) {
-				String c = startNext();
-				try {
-					if (c == null) c = Main.readLine();
-					if (c.equals("`")) {
-						if (!SuggestProcessor.isEmpty()) {
-							SuggestProcessor.run();
-							Main.write("Action done.");
-							continue;
+			
+			if (!disconnected) {
+				while (true) {
+					String c = startNext();
+					try {
+						if (c == null)
+							c = Main.readLine();
+						if (c.equals("`")) {
+							if (!SuggestProcessor.isEmpty()) {
+								SuggestProcessor.run();
+								Main.write("Action done.");
+								continue;
+							}
+						} else if (c.startsWith("!")) {
+							String[] sp = c.split(" ", 2);
+							CmdInfo ci = new ConsoleCmdInfo(sp);
+							if (loopCmd(ci))
+								continue;
 						}
-					} else if (c.startsWith("!")) {
-						String[] sp = c.split(" ", 2);
-						CmdInfo ci = new ConsoleCmdInfo(sp);
-						if (loopCmd(ci))
-							continue;
+						if (!c.isEmpty()) {
+							bot.sendIRC().message(Chat.getDefCh(), c);
+							Main.write("* " + Chat.getName() + ": " + c);
+						}
+					} catch (UserInterruptException e) {
+						break;
+					} catch (EndOfFileException e) {
+						break;
 					}
-					if (!c.isEmpty()) {
-						bot.sendIRC().message(Chat.getDefCh(), c);
-						Main.write("* " + Chat.getName() + ": " + c);
-					}
-				} catch (UserInterruptException e) {
-					break;
-				} catch (EndOfFileException e) {
-					break;
 				}
+				bot.stopBotReconnect();
+				bot.close();
 			}
-			bot.stopBotReconnect();
-			bot.close();
 		}
 
 		private boolean loopCmd(CmdInfo inf) {
@@ -97,10 +101,19 @@ public class Chat {
 				}
 			}
 		}
+
+		private boolean disconnected = false;
+
+		@Override
+		public void onDisconnect(DisconnectEvent event) throws Exception {
+			Main.writeWarn("DISCONNECTED: " + event.getDisconnectException().getMessage());
+			Main.writeWarn("Will try to reconnect...");
+			disconnected = true;
+		}
 	}
-	
+
 	private static Iterator<String> startcmd = null;
-	
+
 	private static String startNext() {
 		if (startcmd != null && startcmd.hasNext())
 			return startcmd.next();
@@ -123,7 +136,7 @@ public class Chat {
 
 		bot = new PircBotX(new Configuration.Builder().addServer(SERVER, PORT)
 				.setSocketFactory(SSLSocketFactory.getDefault()).setName(name).setServerPassword(auth)
-				.addAutoJoinChannel(defch).addListener(new Listener()).buildConfiguration());
+				.addAutoJoinChannel(defch).addListener(new Listener()).setAutoReconnect(true).buildConfiguration());
 	}
 
 	public static PircBotX getBot() {
